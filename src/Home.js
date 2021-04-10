3a8a0b616e795f7b647e9c5218dc7c71099a2dcc
iwanjo <kamanuian@gmail.com>
2021-04-11 01:31:33 +0200

    routing

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import ScrollToTop from "react-scroll-to-top";
 import Fade from 'react-reveal/Fade';
-import { BrowserRouter as Link}	from "react-router-dom";
+import { Route, BrowserRouter as Router, Link}	from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
